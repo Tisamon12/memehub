13941d9fde144b3a15b40344d09a358254244985
Michał Krakowiak <ja.krakowiak@gmail.com>
2020-06-29 11:35:14 +0200

    Some fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,17 @@
 class ApplicationController < ActionController::Base
 
 	before_action :configure_permitted_parameters, if: :devise_controller?
+	before_action :new_user, unless: :user_signed_in?
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
+
+  def new_user
+  	@user = User.new
   end
 
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,6 +3,7 @@ class Vote < ApplicationRecord
   belongs_to :resource, polymorphic: true
 
   after_save :change_meme_rate
+  after_destroy :restore_meme_rate
 
   def self.rate(resource_id, resource_type)
   	Vote.where(resource_id: resource_id, resource_type: resource_type, is_upvote: true).count - Vote.where(resource_id: resource_id, resource_type: resource_type, is_upvote: false).count
@@ -16,4 +17,11 @@ class Vote < ApplicationRecord
   			resource.save
   		end
   	end
+
+    def restore_meme_rate
+      if resource_type == "Meme"
+        is_upvote ? resource.rate -= 1 : resource.rate += 1
+        resource.save
+      end
+    end
 end
